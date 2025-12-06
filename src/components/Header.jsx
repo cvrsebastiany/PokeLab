8416125b78368fb022b139847fcb066807e02f24
Edgar Oliveira <enosfs2@gmail.com>
2025-12-06 20:47:20 -0300

    feat: update header logo to use an image and adjust logo size in CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
   return (
     <header className="header">
       <div className="header-left">
-        <div className="logo">⚡</div>
+        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e8ddc4da-23dd-4502-b65b-378c9cfe5efa/dffpqjc-645c8699-888a-4f3b-b037-418d4ee0f6be.png/v1/fill/w_1280,h_1280/pokemon_center_logo_by_jormxdos_dffpqjc-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MCIsInBhdGgiOiIvZi9lOGRkYzRkYS0yM2RkLTQ1MDItYjY1Yi0zNzhjOWNmZTVlZmEvZGZmcHFqYy02NDVjODY5OS04ODhhLTRmM2ItYjAzNy00MThkNGVlMGY2YmUucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.uBCZnpD-O359RLab8MCpWzJS296c1DXQRabuTLvlOSM" alt="Pokemon Center Logo" className="logo" />
         <h1>PokéLab</h1>
       </div>
       <div className="header-right">
