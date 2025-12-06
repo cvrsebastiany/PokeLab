cdceb710cfa7032eaf0b43a8fbd6b071cc51b98c
Handriel Scheffer <handriel.scheffer@mca.ufcspa.edu.br>
2025-12-06 07:37:35 -0300

    Adiciona react-router-dom e implementa rotas para a página PokemonClinic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import PokemonClinic from './pages/PokemonClinic'
 import './App.css'
 
 function App() {
-  return <PokemonClinic />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/pokelab" element={<PokemonClinic />} />
+        <Route path="/" element={<Navigate to="/pokelab" replace />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
